db79ab2082dfcb98363cc43f81f487534e9884a3
amin m <sudanamin@hotmail.com>
2015-04-17 20:02:57 +0400

    version1 nbranch navigater

--- src/com/androidhive/pushnotifications/AndroidMobileAppSampleActivity.java
+++ src/com/androidhive/pushnotifications/AndroidMobileAppSampleActivity.java
@@ -2,6 +2,7 @@ package com.androidhive.pushnotifications;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -9,12 +10,13 @@ import android.webkit.WebViewClient;
 
 public class AndroidMobileAppSampleActivity extends Activity {
     /** Called when the activity is first created. */
+	WebView mainWebView;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
-        WebView mainWebView = (WebView) findViewById(R.id.mainWebView);
+        mainWebView = (WebView) findViewById(R.id.mainWebView);
         
         WebSettings webSettings = mainWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
@@ -23,7 +25,26 @@ public class AndroidMobileAppSampleActivity extends Activity {
         mainWebView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
         
         mainWebView.loadUrl("http://baby-me.weebly.com/");
+
+
+////////////////////////////////////////////////
+
+
     }
+    
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // Check if the key event was the Back button and if there's history
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && mainWebView.canGoBack()) {
+            mainWebView.goBack();
+            return true;
+        }
+        // If it wasn't the Back key or there's no web page history, bubble up to the default
+        // system behavior (probably exit the activity)
+        return super.onKeyDown(keyCode, event);
+    }
+
+    /////////////////////////////////////////////////
     
     private class MyCustomWebViewClient extends WebViewClient {
         @Override
@@ -33,3 +54,4 @@ public class AndroidMobileAppSampleActivity extends Activity {
         }
     }
 }
+
